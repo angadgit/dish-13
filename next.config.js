7536ea243a56319298dfd4791ad2197f659621b7
angadgit <angadgupta214@gmail.com>
2023-01-10 11:09:55 +0530

    next config env update

--- next.config.js
+++ next.config.js
@@ -4,10 +4,13 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  // serverMiddleware: [
-  //   // ... other middlewares
-  //   './middlewares'
-  // ]
-}
+  env: {
+    SECRET_KEY: "OJuZVgT6cGSz9tq7Xz0BtQactb9n7AuVnA4Nvsan3KQ=",
+    MONGODB_URI:
+      "mongodb+srv://angad:Angad1234@cluster0.5xl6h.mongodb.net/dishaDB?retryWrites=true&w=majority",
+    SendEmail: "angadg00@gmail.com",
+    SendPassword: "cnsuyuohviztftxl",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
